2ad4c982347b0e8d97de93ae56eef5cfc9049629
zhouyu <1445190294@qq.com>
2023-05-14 00:44:11 +0800

    第四章习题8

---
+++ test04_08.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct pizza
+{
+  string company_name;
+  int radius;
+  double weight;
+};
+int main()
+{
+    pizza * _pizza = new pizza;
+    cout << "please input company's name: " << endl;
+    getline(cin, _pizza->company_name);
+    cout << "please input radius: " << endl;
+    cin >> _pizza->radius;
+    cout << "please input weight: " << endl;
+    cin >> _pizza->weight;
+    cout << "company's name: " << _pizza->company_name
+    << "; radius: " << _pizza->radius
+    << "; weight: " << _pizza->weight;
+    delete _pizza;
+    return 0;
+}
