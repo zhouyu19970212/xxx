6495fd610ca666f377e9f9a9665034928e0130e9
zhouyu <1445190294@qq.com>
2023-05-14 00:44:11 +0800

    第四章习题6

---
+++ test04_06.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct CandyBar 
+{
+  string brand;
+  double weight;
+  int calorie;  
+};
+int main()
+{
+    struct CandyBar sandy[3];
+    for(int i = 0; i < 3; i++)
+    {
+      sandy[i].brand = "None";
+      sandy[i].weight = 0.0;
+      sandy[i].calorie = 0;
+      cout << "brand: " << sandy[i].brand 
+      << " weight: " << sandy[i].weight
+      << " calorie: " << sandy[i].calorie << endl;
+    }
+    return 0;
+}
