43fe790c2185d817b678949569427d1e9d3a0f69
zhouyu <1445190294@qq.com>
2023-05-14 00:44:11 +0800

    第七章习题9

---
+++ test07_09.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+double add(double x, double y);
+double calculate(double x, double y, double (*pf) (double a, double b));
+
+int main()
+{
+    double res = calculate(2.5, 10.4, add);
+    cout << "res = " << res << endl;
+    return 0;
+}
+
+double add(double x, double y)
+{
+    return x + y;
+}
+
+double calculate(double x, double y, double (*pf) (double a, double b))
+{
+    // pf(x, y) or (*pf) (x, y)
+    return pf(x, y);
+}
