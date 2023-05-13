74fec4365c2bfa219788f8bb1b8f53c066a34441
zhouyu <1445190294@qq.com>
2023-05-13 11:16:48 +0800

    第四章习题2

---
+++ test04_02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string first_name;
+    string last_name;
+    int age = 0;
+    char grade;
+    cout << "What is your first name? ";
+    getline(cin, first_name);
+    cout << "What is your last name? ";
+    getline(cin, last_name);
+    cout << "What letter grade do you deserve? ";
+    cin >> grade;
+    cout << "What is your age? ";
+    cin >> age;
+    cout << "Name: " << last_name << ", " << first_name << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Age: " << age << endl;
+}
